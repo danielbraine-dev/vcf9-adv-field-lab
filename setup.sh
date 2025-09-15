01a2346859fd87c8ac5ce9414fbe02b8f907a5db
Ben Stein <ben.stein@broadcom.com>
2025-09-15 10:46:41 -0400

    changing execution spot of dns_fix and add context for VKS workload cluster

--- setup.sh
+++ setup.sh
@@ -21,8 +21,14 @@ while IFS= read -r command; do
     fi
 done < "$COMMANDS_FILE"
 
+echo "Applying DNS fix..."
+./dns_fix.sh
+
 echo "Creating Supervisor context..."
 vcf context create sup-wld --endpoint 10.1.0.2 -u administrator@wld.sso --insecure-skip-tls-verify --type k8s --auth-type basic
+
+echo "Creating VKS workload cluster context..."
+vcf context create vks-cluster-qxml --endpoint 10.1.0.2 -u administrator@wld.sso --insecure-skip-tls-verify --workload-cluster-name kubernetes-cluster-qxml --workload-cluster-namespace demo-namespace-vkrcg --type k8s --auth-type basic
 
 echo "Setting sup-wld context as current..."
 vcf context use sup-wld
