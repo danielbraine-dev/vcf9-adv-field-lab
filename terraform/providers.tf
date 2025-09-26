7c72c9bbf57b71d59f3c20ea5f789cd084aaad4c
danielbraine-dev <daniel.braine@broadcom.com>
2025-09-26 08:32:03 -0700

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -28,10 +28,14 @@ terraform {
 
 # --- NSX-T (active) ---
 provider "nsxt" {
-  host                 = nsx-wld01-a.site-a.vcf.lab
-  username             = admin
-  password             = VMware123!VMware123!
-  allow_unverified_ssl = true
+  host                  = nsx-wld01-a.site-a.vcf.lab
+  username              = admin
+  password              = VMware123!VMware123!
+  allow_unverified_ssl  = true
+  max_retries           = 10
+  retry_min_delay       = 500
+  retry_max_delay       = 5000
+  retry_on_status_codes = [429]
 }
 
 # --- AVI ---
