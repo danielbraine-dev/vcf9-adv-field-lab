f8ac6eeb1cde4d0d39a62c499c96ffc3af651e48
danielbraine-dev <daniel.braine@broadcom.com>
2025-09-27 02:17:35 -0400

    Create provider.tf

---
+++ terraform/clean_and_stage/provider.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    nsxt = {
+      source  = "vmware/nsxt"
+      version = ">= 3.9.0"
+    }
+    avi = {
+      source  = "vmware.com/avi/avi"
+      version = "31.1.1"
+    }
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = ">= 2.5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.29.0"
+    }
+    vcfa = {
+      source  = "vmware/vcfa"
+      version = ">= 1.0.0"
+    }
+  }
+}
+
+# --- NSX-T ---
+provider "nsxt" {
+  host                  = var.nsx_host
+  username              = var.nsx_username
+  password              = var.nsx_password
+  allow_unverified_ssl  = var.nsx_allow_unverified_ssl
+  max_retries           = 10
+  retry_min_delay       = 500
+  retry_max_delay       = 5000
+  retry_on_status_codes = [429]
+}
+
+# --- Avi (NSX ALB) ---
+provider "avi" {
+  avi_username   = var.avi_username
+  avi_password   = var.avi_password
+  avi_tenant     = var.avi_tenant
+  avi_controller = var.avi_controller
+  avi_version    = var.avi_version
+}
+
+# --- vSphere ---
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+# --- Kubernetes (optional; uncomment and set vars to use) ---
+# provider "kubernetes" {
+#   config_path    = var.kubeconfig_path
+#   config_context = var.kube_context
+# }
+
+# --- VCFA ---
+provider "vcfa" {
+  endpoint = var.vcfa_endpoint
+  token    = var.vcfa_token
+}
