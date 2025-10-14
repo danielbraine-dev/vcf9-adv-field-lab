a2bbd933a6bf8aae73ca372b1509d3a99555b71d
danielbraine-dev <daniel.braine@broadcom.com>
2025-10-14 12:54:30 -0700

    Update oda_deploy.tf

--- terraform/offline-depot/oda_deploy.tf
+++ terraform/offline-depot/oda_deploy.tf
@@ -2,24 +2,34 @@
 # ODA Controller OVA deploy (vSphere)
 ############################
 
-variable "vsphere_cluster"  { type = string }
-variable "vsphere_datastore"{ type = string }
-
-variable "oda_ova_path"     { type = string }
-variable "oda_vm_name"      { type = string }
-variable "oda_mgmt_pg"      { type = string }
-variable "oda_mgmt_ip"      { type = string }
-variable "oda_mgmt_netmask" { type = string }
-variable "oda_mgmt_gateway" { type = string }
-variable "oda_dns_servers"  { type = list(string) }
-variable "oda_ntp_servers"  { type = list(string) }
-variable "oda_domain_search"{ type = string }
-variable "oda_admin_password" { 
-  type = string
-  sensitive = true 
+############################
+# Base vSphere Variables
+############################
+variable "vsphere_datacenter" {
+  description = "Target datacenter name"
+  type        = string
+  default     = "dc-a"
+}
+variable "vsphere_cluster" {
+  description = "Target cluster name"
+  type        = string
+  default     = "cluster-mgmt-01a"
+}
+variable "vsphere_datastore"{ 
+  description = "Target datastore"
+  type        = string 
+  default     = "vsan-mgmt-01a"
+}
+variable "vsphere_mgmt_pg"{ 
+  description = "Target dvPg for mgmt appliances"
+  type        = string 
+  default     = "mgmt-vds-01-mgmt-01a"
 }
 
-# Inventory lookups
+############################
+# vSphere Inventory lookups
+############################
+
 data "vsphere_datacenter" "oda_dc" {
   name = var.vsphere_datacenter
 }
@@ -35,23 +45,42 @@ data "vsphere_datastore" "oda_ds" {
 }
 
 data "vsphere_network" "oda_net" {
-  name          = var.oda_mgmt_pg
+  name          = var.vsphere_mgmt_pg
   datacenter_id = data.vsphere_datacenter.oda_dc.id
 }
 
-data "vsphere_resource_pool" "oda_rp" {
-  name          = "${data.vsphere_compute_cluster.oda_cluster.name}/Resources"
-  datacenter_id = data.vsphere_datacenter.oda_dc.id
+############################
+# OVA Variable Insertion
+############################
+
+variable "oda_ova_path"     { type = string }
+variable "oda_vm_name"      { 
+  type          = string
+  default       = "vcf-offline-depot-appliance-0.1.3"
+}
+variable "oda_mgmt_ip"      { type = string }
+variable "oda_mgmt_netmask" { type = string }
+variable "oda_mgmt_gateway" { type = string }
+variable "oda_dns_servers"  { type = list(string) }
+variable "oda_ntp_servers"  { type = list(string) }
+variable "oda_domain_search"{ type = string }
+variable "oda_admin_password" { 
+  type = string
+  sensitive = true 
 }
 
-# vSphere: Local Content Library for oda SE
+############################
+# vSphere: Local Content Library for ODA appliance
+############################
 resource "vsphere_content_library" "oda_se_cl" {
-  name            = "oda SE Content Library"
-  description     = "Local content library for oda SE artifacts"
+  name            = "ODA Content Library"
+  description     = "Local content library for ODA artifacts"
   storage_backing = [data.vsphere_datastore.oda_ds.id]
 }
 
-# Deploy OVA
+############################
+# Deploy Offline Depot Appliance OVA
+############################
 resource "vsphere_virtual_machine" "oda_controller" {
   name             = var.oda_vm_name
   datastore_id     = data.vsphere_datastore.oda_ds.id
