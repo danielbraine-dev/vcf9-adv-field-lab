1b2f05e5d891e2f6655b161f173972bf103d4477
danielbraine-dev <daniel.braine@broadcom.com>
2025-10-14 12:05:56 -0700

    Update providers.tf

--- terraform/offline-depot/providers.tf
+++ terraform/offline-depot/providers.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    vsphere = {
+      source  = "vmware/vsphere"
+      version = ">= 2.15.0"
+    }
+  }
+}
+
+# --- vSphere ---
+provider "vsphere" {
+  user                 = "administrator@vshpere.local"
+  password             = "VMware123!VMware123!
+  vsphere_server       = "vc-mgmt-a.site-a.vcf.lab"
+  allow_unverified_ssl = true
+}
+
+
+
+
+
 
