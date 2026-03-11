6ab7004ccf167c9aa03050d21cd3d22e0d7f3f8a
danielbraine-dev <daniel.braine@broadcom.com>
2026-03-11 14:44:56 -0400

    Create supervisor.tf

---
+++ terraform/clean_and_stage/supervisor.tf
@@ -0,0 +1,55 @@
+#########################################################
+# Supervisor Data Lookups
+#########################################################
+
+data "vsphere_compute_cluster" "wld01" {
+  name          = "cluster-wld01-01a"
+  datacenter_id = data.vsphere_datacenter.avi_dc.id
+}
+
+data "vsphere_storage_policy" "vsan_default" {
+  name = "vSAN Default Storage Policy"
+}
+
+# Lookup for the vSphere Zone
+data "vsphere_zone" "wld_a" {
+  name = "z-wld-a"
+}
+
+#########################################################
+# vSphere Supervisor Deployment (VCF with Networking VPC)
+#########################################################
+
+resource "vsphere_supervisor" "wld01_sup" {
+  cluster        = data.vsphere_compute_cluster.wld01.id
+  storage_policy = data.vsphere_storage_policy.vsan_default.id
+  zone_id        = data.vsphere_zone.wld_a.id
+
+  # Matches your first screenshot
+  management_network {
+    network      = data.vsphere_network.avi_net.id # mgmt-vds01-wld01-01a
+    network_mode = "STATIC"
+    ip_ranges {
+      starting_address = "10.1.1.85"
+      ip_count         = 11
+    }
+    subnet_mask        = "255.255.255.0"
+    gateway            = "10.1.1.1"
+    dns_servers        = ["10.1.1.1"]
+    dns_search_domains = ["site-a.vcf.lab"]
+    ntp_servers        = ["10.1.1.1"]
+  }
+
+  # Matches your second screenshot (VPC Networking)
+  namespaces_network_provider {
+    nsx_vcf_vpc {
+      project_id                  = data.nsxt_policy_project.default_project.id
+      vpc_connectivity_profile_id = nsxt_policy_vpc_connectivity_profile.default_vpc_profile.id
+      transit_gateway_ip_block    = "172.16.101.0/24"
+      vpc_cidrs                   = ["172.16.201.0/24"]
+      service_cidr                = "10.96.0.0/23"
+      dns_servers                 = ["10.1.1.1"]
+      ntp_servers                 = ["10.1.1.1"]
+    }
+  }
+}
