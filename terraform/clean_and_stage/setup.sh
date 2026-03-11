a668482f3bece43ced278fba1e75b97807c4a8a8
danielbraine-dev <daniel.braine@broadcom.com>
2026-03-11 00:36:23 -0400

    Update setup.sh

--- terraform/clean_and_stage/setup.sh
+++ terraform/clean_and_stage/setup.sh
@@ -118,8 +118,8 @@ step5_deploy_avi(){
 
   # 1. Apply Terraform to build the Resource Pool & Content Library ONLY
   terraform -chdir="${ROOT_DIR}" apply -auto-approve \
-    -target ='vsphere_resource_pool.avi' \
-    -target ='vsphere_content_library.avi_se_cl'
+    -target='vsphere_resource_pool.avi' \
+    -target='vsphere_content_library.avi_se_cl'
 
   # 2. Locate the OVA
   FINAL_OVA_PATH=$(ls -1 "${ROOT_DIR}"/*.ova 2>/dev/null | head -n1)
