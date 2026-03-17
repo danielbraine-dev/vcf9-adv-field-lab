2a699b47c54cd0ac5f65639129d383ecf95a41a3
danielbraine-dev <daniel.braine@broadcom.com>
2026-03-17 14:56:19 -0400

    Update deploy_supervisor.py

--- terraform/clean_and_stage/scripts/deploy_supervisor.py
+++ terraform/clean_and_stage/scripts/deploy_supervisor.py
@@ -75,7 +75,7 @@ def deploy_supervisor(token, morefs):
             "network": {
                 "network": morefs["network"],
                 "backing": {
-                    "backing": "VSPHERE_DVS"
+                    "backing": "VSPHERE"
                 },
                 "services": {
                     "dns": {
