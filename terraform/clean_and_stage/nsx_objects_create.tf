3dc12b91277d7bb7aa5f2e0805dd35ebf2a8889e
danielbraine-dev <daniel.braine@broadcom.com>
2026-05-27 11:58:22 -0400

    Update nsx_objects_create.tf

--- terraform/clean_and_stage/nsx_objects_create.tf
+++ terraform/clean_and_stage/nsx_objects_create.tf
@@ -2,33 +2,24 @@
 # NSX-T: Shared Services VPC & Subnets
 ############################
 
-# 1. Look up the default Project
-data "nsxt_policy_project" "default" {
-  display_name = "default"
-}
-
-# 2. The Shared Services VPC
+# 1. The Shared Services VPC
 resource "nsxt_vpc" "shared_services" {
   display_name = "Shared-Services"
   description  = "VPC hosting the Avi Load Balancer Infrastructure"
   
-  context {
-    project_id = data.nsxt_policy_project.default.id
-  }
+  # By omitting the 'context' block, NSX automatically deploys this 
+  # into the default infrastructure space and binds it to the default TGW.
 }
 
-# 3. VPC Subnet: SE-mgmt
+# 2. VPC Subnet: SE-mgmt
 resource "nsxt_vpc_subnet" "se_mgmt" {
   display_name = "SE-mgmt"
   
-  context {
-    project_id = data.nsxt_policy_project.default.id
-    vpc_id     = nsxt_vpc.shared_services.id
-  }
+  # NSX policy resources link via "path", not "id"
+  vpc_path     = nsxt_vpc.shared_services.path
   
   access_mode  = "Public" 
   
-  # NSX IPAM will automatically calculate the DHCP range based on this /25
   ip_addresses = ["10.4.100.254/25"]
   
   dhcp_config {
@@ -36,18 +27,14 @@ resource "nsxt_vpc_subnet" "se_mgmt" {
   }
 }
 
-# 4. VPC Subnet: SE-Data_VIP
+# 3. VPC Subnet: SE-Data_VIP
 resource "nsxt_vpc_subnet" "se_data_vip" {
   display_name = "SE-Data_VIP"
   
-  context {
-    project_id = data.nsxt_policy_project.default.id
-    vpc_id     = nsxt_vpc.shared_services.id
-  }
+  vpc_path     = nsxt_vpc.shared_services.path
   
   access_mode  = "Public"
   
-  # NSX IPAM will automatically calculate the DHCP range based on this /25
   ip_addresses = ["10.4.100.126/25"]
   
   dhcp_config {
